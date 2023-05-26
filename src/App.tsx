b4e18ef073e44bf229795e1bfb9b73314d55390d
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-26 22:57:01 +0300

    nav bar clickable links

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route}  from "react-router-dom";
+
 
 function App() {
-  return <Nav />;
+  return (
+    <Router>
+
+      <Nav />
+      <div style={{ height: "60vh", width: "100%", background: "linear-gradient(to right, red, blue, green)" }}></div>
+      <div style={{ height: "60vh", width: "100%", background: "red" }}></div>
+      <div style={{ height: "60vh", width: "100%", background: "blue" }}></div>
+      <div style={{ height: "60vh", width: "100%", background: "green" }}></div>
+      <div style={{ height: "60vh", width: "100%", background: "yellow" }}></div>
+      <div style={{ height: "60vh", width: "100%", background: "white" }}></div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import AppleLogoGrey from "../images/apple-logo-grey.png";
 import SearchIcon from "@mui/icons-material/Search";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 type Props = {};
@@ -10,19 +10,39 @@ function Nav({}: Props) {
   return (
     <nav id="nav" className="navbar">
       <div className="nav__container">
-        <div className="logo__wrapper">
+        <Link to="/" className="logo__wrapper">
           <img src={AppleLogoGrey} alt="" />
-        </div>
-        <li className="nav__listItem">Store</li>
-        <li className="nav__listItem">Mac</li>
-        <li className="nav__listItem">iPad</li>
-        <li className="nav__listItem">iPhone</li>
-        <li className="nav__listItem">Watch</li>
-        <li className="nav__listItem">AirPods</li>
-        <li className="nav__listItem">TV & Home</li>
-        <li className="nav__listItem">Entertainment</li>
-        <li className="nav__listItem">Accessories</li>
-        <li className="nav__listItem">Support</li>
+        </Link>
+        <Link to="/store/">
+          <li className="nav__listItem">Store</li>
+        </Link>
+        <Link to="/mac/">
+          <li className="nav__listItem">Mac</li>
+        </Link>
+        <Link to="/ipad/">
+          <li className="nav__listItem">iPad</li>
+        </Link>
+        <Link to="/iphone/">
+          <li className="nav__listItem">iPhone</li>
+        </Link>
+        <Link to="/watch/">
+          <li className="nav__listItem">Watch</li>
+        </Link>
+        <Link to="/airpods">
+          <li className="nav__listItem">AirPods</li>
+        </Link>
+        <Link to="/tv-home/">
+          <li className="nav__listItem">TV & Home</li>
+        </Link>
+        <Link to="/entertainment">
+          <li className="nav__listItem">Entertainment</li>
+        </Link>
+        <Link to="/accessories">
+          <li className="nav__listItem">Accessories</li>
+        </Link>
+        <Link to="/support">
+          <li className="nav__listItem">Support</li>
+        </Link>
         <li className="nav__listItem">
           <SearchIcon />
         </li>
