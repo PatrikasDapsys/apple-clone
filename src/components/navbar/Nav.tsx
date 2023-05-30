713761fbc0d73948ed0100deb1d285fae80eecf7
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-30 23:19:50 +0300

    working on nav hover

--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -7,6 +7,7 @@ import "./Nav.css";
 type Props = {};
 
 function Nav({}: Props) {
+
   return (
     <nav id="nav" className="navbar">
       <div className="nav__container">
@@ -19,15 +20,41 @@ function Nav({}: Props) {
             <li className="nav__listItem">Store</li>
           </Link>
           <div className="dropdown-menu">
-            <li>Ac 1</li>
-            <li>Ac 2</li>
-            <li>Ac 3</li>
+            <div className="dropdown-menu__container">
+              <div className="dropdown__main">
+                <div className="dropdown-menu__sectionTitle">Shop</div>
+                <li>Shop the latest</li>
+                <li>Mac</li>
+                <li>iPad</li>
+                <li>iPhone</li>
+                <li>Apple Watch</li>
+                <li>Accessories</li>
+              </div>
+              <div className="dropdown__secondary">
+                <div className="dropdown-menu__sectionTitle">Quick Links</div>
+                <li>Find a Store</li>
+                <li>Order Status</li>
+                <li>Apple Trade in</li>
+                <li>Financing</li>
+              </div>
+              <div className="dropdown__secondary">
+                <div className="dropdown-menu__sectionTitle">
+                  Shop Special Stores
+                </div>
+                <li>Certified Refurbished</li>
+                <li>Education</li>
+                <li>Business</li>
+                <li>Veterans and Military</li>
+                <li>Goverment</li>
+              </div>
+            </div>
           </div>
         </div>
 
         <Link to="/mac/">
           <li className="nav__listItem">Mac</li>
         </Link>
+
         <Link to="/ipad/">
           <li className="nav__listItem">iPad</li>
         </Link>
