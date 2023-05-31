12420edbf2ca857783a70fb1f2ac002794be7603
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-31 23:33:32 +0300

    working on footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Nav from "./components/navbar/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,75 @@
+import "./Footer.css";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <section id="footer">
+      <div className="footer__container">
+        <div className="footer__rules">
+          <p>
+            1. Trade-in values will vary based on the condition, year, and
+            configuration of your eligible trade-in device. Not all devices are
+            eligible for credit. You must be at least 18 years old to be
+            eligible to trade in for credit or for an Apple Gift Card. Trade-in
+            value may be applied toward qualifying new device purchase or added
+            to an Apple Gift Card. Actual value awarded is based on receipt of a
+            qualifying device matching the description provided when the
+            estimate was made. Sales tax may be assessed on the full value of a
+            new device purchase. In-store trade-in requires the presentation of
+            a valid photo ID (local law may require saving this information).
+            The offer may not be available in all stores and may vary between
+            in-store and online trade-in. Some stores may have additional
+            requirements. Apple or its trade-in partners reserve the right to
+            refuse or limit the quantity of any trade-in transaction for any
+            reason. More details are available from Apple’s trade-in partner for
+            the trade-in and recycling of eligible devices. Restrictions and
+            limitations may apply.
+            <br />
+            <br />
+            To access and use all the features of Apple Card, you must add Apple
+            Card to Wallet on an iPhone or iPad with the latest version of iOS
+            or iPadOS. Update to the latest version by going to Settings &gt;
+            General &gt; Software Update. Tap Download and Install.
+            <br />
+            <br />
+            Available for qualifying applicants in the United States.
+            <br />
+            <br />
+            Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City
+            Branch.
+            <br />
+            <br />
+            Learn more about how Apple Card applications are evaluated at{" "}
+            <span
+              style={{ textDecoration: "underline", filter: "brightness(70%)" }}
+            >
+              <a href="https://support.apple.com/en-us/HT209218">
+                support.apple.com/kb/HT209218.
+              </a>
+            </span>
+            <br />
+            <br />
+            A subscription is required for Apple TV+.
+            <br />
+            <br />
+            Major League Baseball trademarks and copyrights are used with
+            permission of MLB Advanced Media, L.P. All rights reserved.
+          </p>
+        </div>
+        <div className="footer__divider--horizontal"></div>
+        <div className="footer__links">
+          <li>dasda</li>
+          <li>dasda</li>
+          <li>dasda</li>
+          <li>dasda</li>
+          <li>dasda</li>
+        </div>
+        <div className="footer__divider--horizontal"></div>
+        <div className="footer__legal">legallllllllllllllllllll</div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
