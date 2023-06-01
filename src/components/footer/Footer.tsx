547d892264e9b43e479b1a33d148e4bb1bffcdc8
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-06-01 21:23:55 +0300

    footer done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -59,14 +59,144 @@ function Footer({}: Props) {
         </div>
         <div className="footer__divider--horizontal"></div>
         <div className="footer__links">
-          <li>dasda</li>
-          <li>dasda</li>
-          <li>dasda</li>
-          <li>dasda</li>
-          <li>dasda</li>
+          <div className="footer__linksColumn">
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Shop and learn</div>
+              <li>Store</li>
+              <li>Mac</li>
+              <li>iPad</li>
+              <li>iPhone</li>
+              <li>Watch</li>
+              <li>AirPods</li>
+              <li>TV & Home</li>
+              <li>AirTag</li>
+              <li>Accessories</li>
+              <li>Gift Cards</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Apple Wallet</div>
+              <li>Wallet</li>
+              <li>Apple Card</li>
+              <li>Apple Pay</li>
+              <li>Apple Pay</li>
+            </ul>
+          </div>
+
+          <div className="footer__linksColumn">
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Account</div>
+              <li>Manage Your Apple ID</li>
+              <li>Apple Store Account</li>
+              <li>iCloud.com</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Entertainment</div>
+              <li>Apple One</li>
+              <li>Apple TV+</li>
+              <li>Apple Music</li>
+              <li>Apple Arcade</li>
+              <li>Apple Fitness+</li>
+              <li>Apple News+</li>
+              <li>Apple Podcasts</li>
+              <li>Apple Books</li>
+              <li>Apple Store</li>
+            </ul>
+          </div>
+
+          <div className="footer__linksColumn">
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Apple Store</div>
+              <li>Find a Store</li>
+              <li>Genius Bar</li>
+              <li>Today at Apple</li>
+              <li>Apple Camp</li>
+              <li>Apple Store App</li>
+              <li>Certified Refurbished</li>
+              <li>Apple Trade In</li>
+              <li>Financing</li>
+              <li>Carrier Deals at Apple</li>
+              <li>Order Status</li>
+              <li>Shopping Help</li>
+            </ul>
+          </div>
+
+          <div className="footer__linksColumn">
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">For Business</div>
+              <li>Apple for Business</li>
+              <li>Shop for Business</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">For Education</div>
+              <li>Apple and Education</li>
+              <li>Shop for K-12</li>
+              <li>Shop for College</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">For Healthcare</div>
+              <li>Apple in Healthcare</li>
+              <li>Health on Apple Watch</li>
+              <li>Health Records on iPhone</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">For Government</div>
+              <li>Shop for Government</li>
+              <li>Shop for Veterans and Military</li>
+            </ul>
+          </div>
+
+          <div className="footer__linksColumn">
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">Apple Values</div>
+              <li>Accessibility</li>
+              <li>Education</li>
+              <li>Environment</li>
+              <li>Inclusion and Diversity</li>
+              <li>Privacy</li>
+              <li>Racial Equity and Justice</li>
+              <li>Supplier Responsibility</li>
+            </ul>
+            <ul className="footer__linksColumn--section">
+              <div className="footer__linksTitle">About Apple</div>
+              <li>Apple Leadership</li>
+              <li>Career Opportunities</li>
+              <li>Investors</li>
+              <li>Ethics & Compliance</li>
+              <li>Events</li>
+              <li>Contact Apple</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer__linkExtra">
+          More ways to shop: &nbsp;{" "}
+          <span className="footer__linkExtra--link">Find an Apple Store</span>{" "}
+          &nbsp; or &nbsp;{" "}
+          <span className="footer__linkExtra--link">other retailer</span>&nbsp;
+          near you. &nbsp;
+          <span style={{whiteSpace: "nowrap"}}>
+            Or call 1-800-MY-APPLE.
+          </span>
         </div>
         <div className="footer__divider--horizontal"></div>
-        <div className="footer__legal">legallllllllllllllllllll</div>
+        <div className="footer__legal">
+          <div className="footer__legalLeft" style={{whiteSpace: "nowrap"}}>
+              Copyright © 2023 Apple Inc. All rights reserved.
+          </div>
+
+          <div className="footer__legalMiddle">
+            Privacy Policy
+            <div className="footer__divider--vertical"></div>
+            Terms of Use
+            <div className="footer__divider--vertical"></div>
+            Sales and Refunds
+            <div className="footer__divider--vertical"></div>
+            Legal
+            <div className="footer__divider--vertical"></div>
+            Site Map
+          </div>
+
+          <div className="footer__legalRight">United States</div>
+        </div>
       </div>
     </section>
   );
